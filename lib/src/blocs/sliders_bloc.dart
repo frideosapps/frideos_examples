958403d1993d70fc6d53716d5bd6b948254e10c4
Frideos <frideosapps@gmail.com>
2019-03-22 21:03:02 +0100

    initial commit

---
+++ lib/src/blocs/sliders_bloc.dart
@@ -0,0 +1,30 @@
+import 'package:frideos/frideos.dart';
+
+import '../blocs/bloc.dart';
+
+class SlidersBloc extends BlocBase {
+  SlidersBloc() {
+    angle.value = initialAngle;
+    scale.value = initialScale;
+  }
+
+  final angle = StreamedValue<double>();
+  final scale = StreamedValue<double>();
+
+  double initialAngle = 2.2;
+  double initialScale = 1.8;
+
+  void horizontalSlider(value) {
+    angle.value = value;
+  }
+
+  void verticalSlider(value) {
+    scale.value = value;
+  }
+
+  @override
+  void dispose() {
+    angle.dispose();
+    scale.dispose();
+  }
+}
---
+++ lib/src/blocs/timer_object_bloc.dart
@@ -0,0 +1,29 @@
+import 'package:frideos/frideos.dart';
+
+import '../blocs/bloc.dart';
+
+class TimerObjectBloc extends BlocBase {
+  TimerObjectBloc() {
+    print('-------TimerObject BLOC--------');
+  }
+
+  final timerObject = TimerObject();
+
+  void startTimer() {
+    timerObject.startTimer();
+  }
+
+  void stopTimer() {
+    timerObject.stopTimer();
+  }
+
+  void getLapTime() {
+    timerObject.getLapTime();
+  }
+
+  void dispose() {
+    print('-------TimerObject BLOC DISPOSE--------');
+
+    timerObject.dispose();
+  }
+}
